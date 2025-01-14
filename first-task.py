1eddd281c62c97755a576733f963fd0a97803331
Deepali Jena <jenadeepali47@gmail.com>
2025-01-14 20:13:08 +0530

    first python file with different formatting method

---
+++ first-task.py
@@ -0,0 +1,10 @@
+#DOT method
+username= input("what is the username\n")
+userId=input("what is the userid\n")
+print("user {} created with the id {}".format(username,userId))
+
+
+#F method
+username= input("what is the username\n")
+userId=input("what is the userid\n")
+print(f"user {username} created with the id {userId}")
